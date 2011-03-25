3ae94d8a0e1862d07b5f8e1b115d7da7c51dc25d
Jack Ganzha <jackganzha@gmail.com>
2011-03-25 11:29:15 -0300

    changing code to be more readable

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -2,30 +2,32 @@ class Bowling
 
   def score(moves)
 
-    score = strikes_score(moves)
-    moves = moves.delete("X")
-
+    score  = strikes_score(moves)
     score += spares_score(moves)
-    moves  = moves.gsub(/\d{1}\//, '')
 
-    moves = moves.gsub("-", "0")
+    moves  = clean_moves_from(moves, [/X/, /\d{1}\//, /-/])
+
     score += count_hits(moves) unless moves.empty?
 
     return score
-
   end
 
   def spares_score(moves)
-    moves.count("/") * 15
+    moves.count('/') * 15
   end
 
   def strikes_score(moves)
-    moves.count("X") * 30
+    moves.count('X') * 30
   end
 
   private
     def count_hits(moves)
       moves.chars.collect(&:to_i).inject(&:+)
+    end
+
+    def clean_moves_from(moves, to_remove)
+      to_remove.each { |pattern| moves.gsub!(pattern, '') }
+      moves
     end
 end
 
